796cc92be5d630c630f53d9c3c4e03bbdbede723
Teggert <thomas.eggert@publicispixelpark.de>
2016-12-20 11:32:10 +0100

    init

---
+++ src/assets/js/app.js
@@ -0,0 +1,27 @@
+$(document).foundation(
+    $(window).load(function() {
+        $('.flexslider').flexslider({
+            slideshow: false,
+            animation: "slide",
+            controlNav: false,
+            directionNav: false
+        });
+
+        function sticky()
+        {
+            var window_top=$(window).scrollTop();
+            var top_position=$('body').offset().top;
+            var element_to_stick=$('#header');
+
+            if (window_top > top_position) {
+                element_to_stick.addClass('sticky');
+            } else {
+                element_to_stick.removeClass('sticky');
+            }
+        }
+        $(window).scroll(sticky);
+        sticky();
+
+
+    })
+);
